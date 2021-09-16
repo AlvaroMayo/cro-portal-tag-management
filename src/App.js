67128b802947adf4dfa9eeda67b54f9c79cbf09e
Alvaro Mayo <alvaro.mayo@esure.com>
2021-09-16 12:51:57 +0100

    chore: configure react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./view/landing/landing";
+import Offers from "./view/offers/offers";
+import Policies from "./view/policies/policies";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route path="/policies">
+            <Policies />
+          </Route>
+          <Route path="/offers">
+            <Offers />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
